78746670187926fc617c5fbe08681a23694a229e
alhava <andrew@lhava.io>
2025-03-06 16:39:47 -0500

    update redirect

--- src/oktaConfig.ts
+++ src/oktaConfig.ts
@@ -1,7 +1,7 @@
 const oktaConfig = {
     issuer: 'https://lhava.okta.com/oauth2', // Replace with your Okta domain
     clientId: '0oa1ukln76jvFnS3e1d8', // Replace with your client ID
-    redirectUri: 'http://localhost:5173/login/callback',
+    redirectUri: 'http://13.114.185.3/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true
 }
