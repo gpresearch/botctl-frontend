8d6dd480932121475c0a636de02b50174df272b3
alhava <andrew@lhava.io>
2025-03-06 17:39:34 -0500

    test remove /

--- src/oktaConfig.ts
+++ src/oktaConfig.ts
@@ -1,7 +1,7 @@
 const oktaConfig = {
     issuer: 'https://lhava.okta.com/oauth2',
     clientId: '0oa1ukln76jvFnS3e1d8',
-    redirectUri: 'http://13.114.185.3/login/callback',  // Changed to "/"
+    redirectUri: 'http://13.114.185.3',  // Changed to "/"
     scopes: ['openid', 'profile', 'email'],
     pkce: false
 };
