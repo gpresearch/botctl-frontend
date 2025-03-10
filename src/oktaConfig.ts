8d821d343e4f65deba12aab04529d54a76703a94
alhava <andrew@lhava.io>
2025-03-10 11:54:59 -0400

    remove comments

--- src/oktaConfig.ts
+++ src/oktaConfig.ts
@@ -1,9 +1,9 @@
 const oktaConfig = {
     issuer: 'https://lhava.okta.com/oauth2',
     clientId: '0oa1ukln76jvFnS3e1d8',
-    redirectUri: window.location.origin,  // Changed to "/"
+    redirectUri: window.location.origin,
     scopes: ['openid', 'profile', 'email'],
-    responseType: ['token', 'id_token'], // Fix unsupported_response_type error
+    responseType: ['token', 'id_token'],
     pkce: false
 };
 
