cc1fc7890501bcd0d81ea4b3fd01b21a3405ffcb
alhava <andrew@lhava.io>
2025-03-10 12:03:29 -0400

    handling repsonse types

--- src/oktaConfig.ts
+++ src/oktaConfig.ts
@@ -1,9 +1,11 @@
+import {OAuthResponseType} from "@okta/okta-auth-js";
+
 const oktaConfig = {
     issuer: 'https://lhava.okta.com/oauth2',
     clientId: '0oa1ukln76jvFnS3e1d8',
     redirectUri: window.location.origin,
     scopes: ['openid', 'profile', 'email'],
-    responseType: ['token', 'id_token'],
+    responseType: ['id_token', 'token'] as OAuthResponseType[],
     pkce: false
 };
 
